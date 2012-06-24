14628ebebbf8efb38213a93cdd9079441bf0a160
acerix <dylan@kanux.org>
2012-06-24 21:03:54 +0000

    removed support for multiple files at once

--- upload.php
+++ upload.php
@@ -1,35 +1,31 @@
 <?php
-function rearrange( $arr ) {
-        foreach( $arr as $key => $all ) {
-                foreach( $all as $i => $val ) {
-                        $new[$i][$key] = $val;
-                }
-        }
-        return $new;
-}
-$otps = dirname(__FILE__).'/otps/';
-$random = fopen('/dev/urandom','rb');
 
-if (!empty($_FILES['data']))
-foreach(rearrange($_FILES['data']) as $file) {
-	if ($file['error']!=0) continue;
+if (!empty($_FILES['data']) && $file['error']==0) {
+	
+	$file = $_FILES['data'];
+
+	$otps = dirname(__FILE__).'/otps/';
+
+	$random = fopen('/dev/urandom','rb');
 
 	header('Content-Type: application/octet-stream');
 	header('Content-Disposition: attachment; filename="'.$file['name'].'"');
 	header('Content-Length: '.filesize($file['tmp_name']));
 
-	$uid = sha1_file($file['tmp_name']);
-	$orig = file_get_contents($file['tmp_name']);
+	$uploaded = file_get_contents($file['tmp_name']);
+	
+	$uid = sha1($uploaded);
 
 	if (file_exists($otps.$uid)) {
-		$out = $orig ^ file_get_contents($otps.$uid);
+		$out = $uploaded ^ file_get_contents($otps.$uid);
 	}
 	else {
-		$otp = fread($random,strlen($orig));
-		$out = $orig ^ $otp;
+		$otp = fread($random, strlen($uploaded));
+		$out = $uploaded ^ $otp;
 		$uid = sha1($out);
-		file_put_contents($otps.$uid,$otp);
+		file_put_contents($otps.$uid, $otp);
 	}
+
 	print($out);
 	exit();
 }

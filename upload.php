7188c8d360c55ba34223d434722a758370f337fa
acerix <dylan@kanux.org>
2012-06-25 02:34:23 +0000

    simplified html and php formatting

--- upload.php
+++ upload.php
@@ -1,31 +1,28 @@
 <?php
 
-if (!empty($_FILES['data']) && $_FILES['data']['error']==0) {
-	
+if (!empty($_FILES['data']) && $_FILES['data']['size']) {
+
+	$otp_dir = dirname(__FILE__).'/otps/';
+
 	$file = $_FILES['data'];
-
-	$otps = dirname(__FILE__).'/otps/';
-
 
 	header('Content-Type: application/octet-stream');
 	header('Content-Disposition: attachment; filename="'.$file['name'].'"');
 	header('Content-Length: '.filesize($file['tmp_name']));
 
 	$uploaded = file_get_contents($file['tmp_name']);
-	
+
 	$uid = sha1($uploaded);
 
-	if (file_exists($otps.$uid)) {
-		$out = $uploaded ^ file_get_contents($otps.$uid);
-	}
+	if (file_exists($otp_dir.$uid))
+		print( $uploaded ^ file_get_contents($otp_dir.$uid) );
+
 	else {
 		$otp = file_get_contents('/dev/urandom', NULL, NULL, NULL, strlen($uploaded));
 		$out = $uploaded ^ $otp;
+		print($out);
 		$uid = sha1($out);
-		file_put_contents($otps.$uid, $otp);
+		file_put_contents($otp_dir.$uid, $otp);
 	}
 
-	print($out);
-	exit();
 }
-
